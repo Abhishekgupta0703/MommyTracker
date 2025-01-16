ca1f40290c20ada49eb1fc56bb79a2774c09e6fa
Abhishekgupta0703 <ak19250000@gmail.com>
2025-01-16 21:19:44 +0530

    Date Format Update

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -7,15 +7,22 @@ function ResultContainer({ result, exclude }) {
       <p className="font-bold text-pink-700">{data || "N/A"}</p>
     </div>
   );
-
+  const formatDate = (dateString) => {
+    if (!dateString) return "N/A";
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
   return (
     <div className="bg-pink-100 p-4 mt-4 rounded-lg shadow-lg">
       {result ? (
         <div
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4"
         >
-          {!exclude.includes("dueDate") && renderBox("Due Date", result.dueDate)}
-          {!exclude.includes("lmp") && renderBox("Last Period Date", result.lmp)}
+          {!exclude.includes("dueDate") && renderBox("Due Date", formatDate(result.dueDate))}
+          {!exclude.includes("lmp") && renderBox("Last Period Date", formatDate(result.lmp))}
           {renderBox("Trimester", result.trimester)}
           {renderBox("Current Month", result.month)}
           {renderBox("Current Week", result.week)}
